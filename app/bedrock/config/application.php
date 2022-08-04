363d9b250e37deaff859dfdb8f00c61caea9f8ce
guevarita <guevarita@gmail.com>
2022-08-04 19:30:44 -0300

    enable multisite

--- app/bedrock/config/application.php
+++ app/bedrock/config/application.php
@@ -135,3 +135,15 @@ Config::apply();
 if (!defined('ABSPATH')) {
     define('ABSPATH', $webroot_dir . '/wp/');
 }
+
+/**
+ * Multisite Network
+ */
+define('WP_ALLOW_MULTISITE',true);
+define('MULTISITE', true);
+define('SUBDOMAIN_INSTALL', true); // obv switch this to true if you're using subdomains
+define('DOMAIN_CURRENT_SITE', env('DOMAIN_CURRENT_SITE') ?: parse_url(WP_HOME,  PHP_URL_HOST));
+define('PATH_CURRENT_SITE', '/');
+define('SITE_ID_CURRENT_SITE', 1);
+define('BLOG_ID_CURRENT_SITE', 1);
+
